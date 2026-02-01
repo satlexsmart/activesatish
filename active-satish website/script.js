5917b4b0e41f3688a36c13e15f4ba748ab08ca99
S <satviksinghalyt@gmail.com>
2026-02-01 10:17:29 +0530

    Delete active-satish website directory

--- active-satish website/script.js
+++
@@ -1,9 +0,0 @@
-VanillaTilt.init(document.querySelectorAll(".tilt-card"), {
-    max: 15,          // Max tilt rotation
-    speed: 400,       // Speed of the effect
-    glare: true,      // Adds the shiny light
-    "max-glare": 0.2, // Strength of the shine
-    scale: 1.05       // Zoom effect
-});
-
-console.log("System Active. Welcome, Satish.");
